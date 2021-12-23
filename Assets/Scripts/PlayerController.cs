8888a33a993b577904507b08c566be8c7c3d5888
M1ker1n <m1ker1nigor@yandex.ru>
2021-12-23 17:40:16 +0300

    Merge error fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public struct Bounds
-{
-    public float lower;
-    public float upper;
-}
-
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
